902e57eaa161ff319c40e4cea8a050c72e92a728
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2022-12-27 11:58:22 -0300

    using ngModel at the inputs and at the div's inputs

---
+++ memoteca/src/app/componentes/pensamento/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent {
+  // vamos criar um atributo que se chama pensamento
+  // ele vai ser um objeto de propriedades
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo angular',
+    autoria: 'Dev',
+    modelo: ''
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  criarPensamento(){
+    alert('novo pensamento criado')
+  }
+
+  cancelar(){
+    alert('pensamento cancelado')
+  }
+}
